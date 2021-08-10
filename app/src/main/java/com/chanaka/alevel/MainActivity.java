2b37277764ce0da2dfe0d3df546c20ef26ed45f2
chanakaayesh <chanakaayesh@gmail.com>
2021-08-10 19:59:23 +0200

    this Alevel

--- app/src/main/java/com/chanaka/alevel/MainActivity.java
+++ app/src/main/java/com/chanaka/alevel/MainActivity.java
@@ -1,6 +1,9 @@
 package com.chanaka.alevel;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.view.Menu;
 
@@ -8,6 +11,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,20 +28,52 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    ActionBarDrawerToggle mDrawerToggle;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+
+
         setSupportActionBar(toolbar);
 
+      toolbar.setNavigationIcon(R.drawable.settingsicon);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.settingsicon);
+   //
+        toolbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.ic_settings));
+        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.ic_settings));
+
+
+
+      /*  mDrawerToggle.setDrawerIndicatorEnabled(false);
+        mDrawerToggle.setHomeAsUpIndicator(R.drawable.settingsicon);*/
+      /*  mDrawerToggle =  new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);*/
+     /*   mDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.black));
+        mDrawerToggle.syncState();
+
+        toolbar.setNavigationIcon(null);
+            getSupportActionBar().setIcon(null);*/
+
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
+               navigationView.setItemIconTintList(null);
+
+
+
+               // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_display_timetable, R.id.nav_slideshow,R.id.nav_admin_home)
+                R.id.nav_home, R.id.nav_display_timetable,R.id.nav_admin_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -52,5 +93,17 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
